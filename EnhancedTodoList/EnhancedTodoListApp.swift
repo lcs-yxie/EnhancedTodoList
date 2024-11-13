a7ea2cddef1c1c4eee523df48aae8e028dfe7fac
Yukun Xie <yxie@lcs.on.ca>
2024-11-13 10:36:05 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Yukun Xie on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
